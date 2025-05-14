8d8a2a24a87a94f93ec094b6c99f6e311b3c0e90
tysek <tysek2004@gmail.com>
2025-05-14 07:57:56 +0200

    Improve l7_z2

--- l7/z2.py
+++ l7/z2.py
@@ -1,30 +1,38 @@
 def forall(pred, iterable):
-    return sum([pred(elem) for elem in iterable]) == len(iterable)
+    return not atleast(lambda x: not pred(x), iterable, 1)
 
 def exists(pred, iterable):
-    return sum([pred(elem) for elem in iterable]) > 0
+    return atleast(pred, iterable, 1)
 
 def atleast(pred, iterable, n):
-    return sum([pred(elem) for elem in iterable]) >= n
+    sum = 0
+    for elem in iterable:
+        if pred(elem):
+            sum += 1
+        if sum >= n:
+            return True
+    return False
 
 def atmost(pred, iterable, n):
-    return sum([pred(elem) for elem in iterable]) <= n
+    return not atleast(pred, iterable, n + 1)
 
 if __name__ == '__main__':
     print(forall(lambda x: x % 2 == 0, [2, 4, 5, 8]))
     print(forall(lambda x: x % 2 == 0, [2, 4, 6, 8]))
 
-    print()
-
     print(exists(lambda x: x % 2 == 0, [1, 3, 5, 7]))
     print(exists(lambda x: x % 2 == 0, [2, 4, 5, 8]))
-
-    print()
 
     print(atleast(lambda x: x % 2 == 0, [2, 4, 5, 7], 3))
     print(atleast(lambda x: x % 2 == 0, [2, 4, 5, 8], 3))
 
-    print()
-
     print(atmost(lambda x: x % 2 == 0, [2, 4, 5, 8], 2))
     print(atmost(lambda x: x % 2 == 0, [2, 4, 5, 7], 2))
+
+    import itertools
+
+    print(forall(lambda x: x % 2 == 0, itertools.count(1)))
+    print(exists(lambda x: x % 2 == 0, itertools.count(1)))
+    print(atmost(lambda x: x % 2 == 0, itertools.count(1), 5))
+    print(atleast(lambda x: x % 2 == 0, itertools.count(1), 5))
+    print(forall(lambda x: x % 2 == 0, itertools.count(2, 2)))
