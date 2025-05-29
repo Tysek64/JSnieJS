fa0b1e82ad61529586eb673f11038244368a5739
tysek <tysek2004@gmail.com>
2025-05-29 11:31:33 +0200

    Add l9 z4e

---
+++ l9/tests/ThresholdDetector_test.py
@@ -0,0 +1,11 @@
+import pytest
+import sys
+
+sys.path.append('..')
+sys.path.append('.')
+
+from TimeSeries import TimeSeries
+from Validators import ThresholdDetector
+
+def test():
+    assert 0==0
